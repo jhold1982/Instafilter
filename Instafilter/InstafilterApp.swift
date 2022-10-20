c9d6f7b575cc27b77a368977458a394c5e9c3abd
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-10-19 22:08:25 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Justin Hold on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
